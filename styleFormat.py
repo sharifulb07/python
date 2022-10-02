dc38e813b9b983eb85b6979e4534e9c37fae9f77
shariful islam <sharifulb07@gmail.com>
2022-10-02 10:26:31 +0600

    Initial commit

---
+++ styleFormat.py
@@ -0,0 +1,15 @@
+# num1=58
+# num2=5.90
+# name="This is Shariful Islam Speaking here"
+#
+# print(type (num1))
+# print(type(num2))
+# print(type(name))
+
+
+# print(f"{num1}+{num2}={num1+num2}")
+
+
+print("this shariful islam speaking here , ", end=" ")
+print("01944180957")
+
